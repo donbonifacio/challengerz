ad9e396188034ccfa1683f70cc4713b668ee51f8
Pedro Santos <donbonifacio@gmail.com>
2013-01-09 16:15:57 +0000

    Add html5boilerplate/bootstrap

--- config/routes.js
+++ config/routes.js
@@ -1,7 +1,14 @@
 (function routes() {
 
   exports.load = function load(midgard) {
+
     midgard.route("/events/.+", "events#list");
+
+    midgard.routes.staticAccess([
+      "/css/.+",
+      "/img/.+",
+      "/js/.+"
+    ]);
   };
 
 })();	
