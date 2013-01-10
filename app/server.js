75fa4ffef7fd8d444a245464dcda64153b2bb2b4
Pedro Santos <donbonifacio@gmail.com>
2013-01-10 13:02:27 +0000

    Start localization support

--- app/server.js
+++ app/server.js
@@ -1,5 +1,9 @@
-var midgard = require("midgard.js");
+var midgard = require('midgard.js');
 
 require('../config/routes.js').load(midgard);
+
+midgard.language.setDefaultLanguage('en');
+midgard.language.load('language/pt.json');
+midgard.language.load('language/en.json');
 
 midgard.start(process.env.PORT || 3000);
