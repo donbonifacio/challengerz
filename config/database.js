4c54ea1103b75e6826e6a647df208db541ca0213
Pedro Santos <donbonifacio@gmail.com>
2013-01-15 14:14:22 +0000

    Fix bug

--- config/database.js
+++ config/database.js
@@ -9,7 +9,7 @@
   };
 
   if(!midgard.isDevelopment) {
-    dbconfig[midgard.env] = function fromVars() {
+    dbconfig[midgard.env.NODE_ENV] = function fromVars() {
       return {
         connectionString: process.env.MONGOHQ_URL
       };
