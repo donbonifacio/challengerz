1982683fcae5865ac813fdc9bbc12de75b6d8b81
Pedro Santos <donbonifacio@gmail.com>
2013-01-15 12:09:37 +0000

    Add create/validate EventSource

--- app/models/eventSource.js
+++ app/models/eventSource.js
@@ -1,34 +1,40 @@
 (function EventSource() {
 
+  var database = require('../../lib/database.js');
+
   var EventSource = exports;
 
-  EventSource.validate = function validate(obj, calback) {
+  EventSource.validate = function validate(context, obj, callback) {
+    errors = [];
+    if( !(context.validator.present(obj.slug) && context.validator.string(obj.slug)) ) {
+      errors.push({field: 'slug', error:'Must be present' });
+    }
     if(obj._id) {
       obj.updatedAt = Date.now();
     } else {
       obj.createdAt = Date.now();
     }
+    callback(errors.length == 0, errors);
   };
 
   EventSource.createFromContext = function createFromContext(context, next) {
-    next(context);
-    return;
     context.apiResult = {};
-    var model = JSON.parse(context.response._actual.request.body);
-    console.log(model);
-    EventSource.validate(model, function(valid, errors) {
+    var model = JSON.parse(context.request.body);
+    EventSource.validate(context, model, function(valid, errors) {
       if(valid) {
         context.apiResult.success = true;
-        database.openCollection('eventSources', function(err, callback) {
-          collection.save(model, function(err, calback) {
+        database.openCollection('eventSources', function(err, collection) {
+          collection.save(model, function(err, result) {
             context.apiResult.eventSource = model;
+            next(context);
           });
         });
       } else {
+        context.httpStatus = ;
         context.apiResult.success = false;
         context.apiResult.errors = errors;
+        next(context);
       }
-      next(context);
     });
   };
 
