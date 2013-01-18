d3240cf1c8bbe5058ac6b33c824448d94572ea7a
Pedro Santos <donbonifacio@gmail.com>
2013-01-18 14:04:34 +0000

    Fix event route

--- app/controllers/eventController.js
+++ app/controllers/eventController.js
@@ -2,6 +2,13 @@
 
   exports.layout = "layout#main";
 
+  exports.setup = function setup(context, next) {
+    context.params.eventSourceSlug = context.request.regexParams[1];
+    context.params.eventEdition = context.request.regexParams[2];
+    context.params.eventSlug = context.request.regexParams[3];
+    next(context);
+  };
+
   exports.show = function show(context) {
     context.render();
   };
