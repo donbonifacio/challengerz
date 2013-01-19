bf8bd0a19a93d48ca4bbae479cd2ddcd7121a501
Pedro Santos <donbonifacio@gmail.com>
2013-01-19 16:21:28 +0000

    Add index

--- config/databaseIndexes.js
+++ config/databaseIndexes.js
@@ -1,3 +1,5 @@
+
+db.getCollection('events').ensureIndex({slug:1, edition:1, eventSourceSlug:1});
 db.getCollection('events').ensureIndex({loc:'2d'});
 db.getCollection('events').ensureIndex({createdAt:1});
 
