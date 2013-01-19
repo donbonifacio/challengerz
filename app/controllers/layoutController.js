f3514071ae8153c8e0ab8533fa96154480bb951f
Pedro Santos <donbonifacio@gmail.com>
2013-01-19 11:05:44 +0000

    Refactor top menu

--- app/controllers/layoutController.js
+++ app/controllers/layoutController.js
@@ -1,8 +1,13 @@
 (function layoutController() {
 
   exports.main = function main(context) {
+    context.include('layout#topMenu');
     context.include('midgard#trace');
     context.render();
-  }
+  };
+
+  exports.topMenu = function topMenu(context) {
+    context.render();
+  };
 
 })();
