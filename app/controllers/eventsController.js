f210192774d8a67b9479589763942718fb906f87
Pedro Santos <donbonifacio@gmail.com>
2013-01-21 12:17:22 +0000

    Start main filter

--- app/controllers/eventsController.js
+++ app/controllers/eventsController.js
@@ -3,7 +3,19 @@
   var Event = require('../../app/models/event.js');
   exports.layout = 'layout#main';
 
+  var resolveRequestFilter = function resolveRequestFilter(context) {
+    var filter = {};
+    var url = context.request.url;
+    var parts = midgard._.compact(url.split('/'));
+    parts.shift();
+    while(parts.length > 0 ) {
+      filter[parts.shift()] = parts.shift();
+    }
+    return filter;
+  };
+
   exports.list = function(context) {
+    context.params.eventsFilter = resolveRequestFilter(context);
     context.include('events#latestEvents');
     context.render();
   };
