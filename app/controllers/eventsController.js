229fcb192a50e4ffbc549d08512106b12184c094
Pedro Santos <donbonifacio@gmail.com>
2013-01-23 10:09:23 +0000

    Set events page title

--- app/controllers/eventsController.js
+++ app/controllers/eventsController.js
@@ -1,5 +1,6 @@
 (function eventsController() {
 
+  var EventsController = exports;
   var Event = require('../../app/models/event.js');
   exports.layout = 'layout#main';
 
@@ -20,6 +21,28 @@
     return filter;
   };
 
+  exports.setPageTitle = function setPageTitle(context, next) {
+    var title = context.translate('eventsTitle');
+    if(context.params.eventsFilter.eventTag) {
+      title += " » " + context.translate('tag.'+context.params.eventsFilter.eventTag);
+    }
+    var loc = context.params.eventsFilter.location;
+    if(loc) {
+      var event = midgard._.first(context.fetchLatest);
+      if(loc.length === 1 || loc ) {
+        title += ' » ' + event.country;
+      } 
+      if(loc.length == 2 ) {
+        title += ' » ' + event.region;
+      }
+      if(loc.length == 3 ) {
+        title += ' » ' + event.city;
+      }
+    }
+    context.pageTitle = title;
+    next(context);
+  };
+
   exports.list = function(context) {
     context.params.eventsFilter = resolveRequestFilter(context);
     context.include('events#latestEvents');
@@ -28,6 +51,7 @@
 
   exports.latestEvents = function latestEvents(context) {
     context.dataHandler(Event.fetchLatest);
+    context.dataHandler(EventsController.setPageTitle);
     context.render();
   };
 
